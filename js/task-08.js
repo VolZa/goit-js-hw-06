d5f390500669903fb778668a884247120f505cda
VolZa <vvolza@gmail.com>
2023-01-29 16:50:48 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const formEl = document.querySelector('.login-form');
+const formDataEl = {
+    email: '',
+    password: '',
+};
+
+formEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+        } = event.currentTarget;
+    if (email.value === "" || password.value === "") {
+    return alert("Будьласка заповніть всі поля форми!");
+    }
+    formDataEl.email = email.value;
+    formDataEl.password = password.value;
+    email.value = '';
+    email.password = '';
+
+    // console.log(formDataEl);
+    return formDataEl;
+}
+
 
