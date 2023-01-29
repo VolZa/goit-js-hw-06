0f09e776539a09c4f7e6e412ec81016394516304
VolZa <vvolza@gmail.com>
2023-01-29 19:02:31 +0200

    task9  and create 10

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const refs = {
+  bodyEl:      document.querySelector('body'),
+  buttonEl:    document.querySelector('.change-color'), 
+  spanColorEl: document.querySelector('.color'),
+} 
+
+refs.buttonEl.addEventListener("click", setBgColor);
+
+function setBgColor(event) {
+  const bgColor = getRandomHexColor();
+  refs.bodyEl.style.backgroundColor = bgColor;
+  refs.spanColorEl.textContent =  bgColor;
 }
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  inputEl: document.querySelector('[type="number"]'),
+  createButtonEl: document.querySelector('[data-create]'),
+  destroyButtonEl: document.querySelector('[data-destroy]'),
+  boxesEl: document.querySelector('#boxes')
+}
+
+// console.log(refs.inputEl);
+// console.log(refs.createButtonEl);
+// console.log(refs.destroyButtonEl);
+// console.log(refs.boxesEl);
+
